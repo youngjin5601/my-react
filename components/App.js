494f7791d09164412098e454187d851aa02d8810
youngjin5601 <jin45879@gmai.com>
2020-04-08 01:52:36 +0900

    add numberbaseball

--- components/App.js
+++ components/App.js
@@ -1,37 +1,75 @@
 import React from 'react'
 //import css from '../src/index.css'
 
+const getNumbers=()=>{
+  console.log('hello')
+  const candidates=[1,2,3,4,5,6,7,8,9]
+  const chosens=[]
+  for(let i=0;i<4;i++) chosens.push(candidates.splice(Math.floor(Math.random()*candidates.length),1)[0]);
+  return chosens
+}
 const App=()=>{
-  const [prevWord, setPrevWord]=React.useState('Gabie')
-  const [word, setWord]=React.useState('')
-  const [result, setResult]=React.useState('')
+  const [answer, setAnswer]=React.useState()
+  console.log(answer)
+  const [tryNo, setTryNo]=React.useState(1)
+  const [result,setResult]=React.useState('')
+  const [tryList, setTryList]=React.useState([])
   const inputRef=React.useRef(null)
 
-  const wordInput=(e)=>{
-    setWord(e.target.value)
-  }
-  const wordSubmit=(e)=>{
+  React.useEffect(()=>{
+    setAnswer(getNumbers())
+  },[])
+  const submitForm=(e)=>{
     e.preventDefault()
-    if(prevWord[prevWord.length-1]===word[0]){
-      setPrevWord(word)
-      setResult('result : OK')
+    setTryNo(tryNo+1)
+    if(inputRef.current.value===answer.join('')){
+      setTryList([...tryList, {ans: inputRef.current.value, strike: 4, ball: 0}])
+      inputRef.current.value=''
+      //inputRef.current.focus()
+      setResult('home run!!!')
+      setTimeout(()=>{
+        setTryNo(0)
+        setAnswer(getNumbers)
+        setResult('')
+        setTryList([])
+      },2000)
     }else{
-      setResult('result : try again')
+      let strike=0;
+      let ball=0;
+      for(let i=0;i<answer.length;i++){
+        if(inputRef.current.value[i]==answer[i]){
+          strike++;
+        }else{
+          if(answer.includes(parseInt(inputRef.current.value[i]))) ball++
+        }
+      }
+      setTryList([...tryList, {ans: inputRef.current.value, strike: strike, ball: ball}])
+      inputRef.current.value=''
+      //inputRef.current.focus()
+      if(tryNo>4){
+        setTimeout(()=>{
+          setTryNo(0)
+          setAnswer(getNumbers)
+          setResult('')
+          setTryList([])
+        },2000)
+      }
     }
-    setWord('')
-    inputRef.current.focus()
   }
-  return(
-    <>
-    <h1>Word Relay</h1>
-    <div>{prevWord}</div>
-    <form onSubmit={wordSubmit}>
-      <label htmlFor="relay-word">insert a word</label>
-      <input id="relay-word" ref={inputRef} type="text" value={word} onChange={wordInput}></input>
-      <button>Click!!</button>
+  return(<>
+    <h1>NumberBaseball</h1>
+    <h2>{result}</h2>
+    <form onSubmit={submitForm}>
+      <input ref={inputRef} type="text" maxLength="4" ></input>
+      <button type="submit">click!!</button>
     </form>
-    <div>{result}</div>
-    </>
-  )
+    <ul>
+      {
+        tryList.map((item,idx)=>{
+        return <li key={idx}>ans:{item.ans}- strike:{item.strike}- ball:{item.ball}</li>
+        })
+      }
+    </ul>
+  </>)
 }
 export default App;
