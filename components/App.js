3fd4960cccb667e79f89431f8f2121a5145d0b8a
youngjin5601 <jin45879@gmai.com>
2020-04-11 23:10:54 +0900

    add RPS

--- components/App.js
+++ components/App.js
@@ -1,47 +1,57 @@
 import React from 'react'
-import css from './index.css'
+//import css from './index.css'
+const coord={
+  rock: -215,
+  paper: -430,
+  scissor: 0
+}
+const rsp=(x)=>{
+  return (Object.entries(coord).find(item=>{
+    return item[1]==x
+  })[0])
+}
 
 const App=()=>{
-  const [color, setColor]=React.useState('red')
-  const [message, setMessage]=React.useState('click to start')
-  const [lag, setLag]=React.useState([])
   const id=React.useRef()
-  const start=React.useRef()
-  
-  console.log(start)
-  const clickScreen=()=>{
-    switch(color){
-      case 'red':
-        id.current=setTimeout(() => {
-          setColor('blue')
-          setMessage('click now')
-          start.current=new Date()
-        }, 2000+Math.floor(Math.random()*1000));
-
-        setColor('green')
-        setMessage('wait for blue')
-        break;
-      case 'green':
-        setColor('red')
-        setMessage('you are in a hurry..')
-        clearTimeout(id.current)
-        break;
-      case 'blue':
-        setLag(lag=>[...lag, new Date()-start.current])
-        setColor('red')
-        setMessage('click to start')
-        break;
-    }
+  const [imgCoord, setImagcoord]=React.useState(0)
+  const [choice, setChoice]=React.useState()
+  const changeHand=(sec)=>{
+    return (
+      setTimeout(()=>{
+        if(imgCoord==0){
+          setImagcoord(-215)
+        }else if(imgCoord==-215){
+          setImagcoord(-430)
+        }else{
+          setImagcoord(0)
+        }
+      },sec)
+    )
+  }
+  const stopInterval=()=>{
+    clearTimeout(id.current)
+    changeHand(3000)
+  }
+  React.useEffect(()=>{
+    id.current=changeHand(500)
+  },[imgCoord])
+  const btnClick=(choice)=>{
+    setChoice(choice)
+    clearTimeout(id.current)
+    setTimeout(()=>{
+      setChoice('')
+    },3000)
+    changeHand(3000)
   }
   return(<>
-    <h1>response check</h1>
-    <div id="screen" className={color} onClick={clickScreen}>
-      {message}
-    </div>
-    Records  {lag.length !==0 && lag.reduce((a,b)=>a+b)/lag.length} ms
-      <ul>
-        {lag.map((item,idx)=><li key={idx}>{item} ms</li>)}
-      </ul>
+    <h1>rock, paper, scissor</h1>
+    <div id='rps-img' style={{background: `no-repeat url('../public/rps.jpg') ${imgCoord}px 0`}}></div>
+    <button onClick={stopInterval}>stop : {rsp(imgCoord)}</button>
+    <button>{choice ? choice :'ans'}</button>
+    <hr/>
+    <button onClick={()=>btnClick('rock')}>rock</button>
+    <button onClick={()=>btnClick('paper')}>paper</button>
+    <button onClick={()=>btnClick('scissor')}>scissor</button>
   </>)
 }
 export default App;
